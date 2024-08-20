947a153cecc49b0e7a62e568d13a620fda83b893
Alejandro <jesusmurillo28052@gmail.com>
2024-08-19 22:38:29 -0700

    agregar eventos al proyecto

---
+++ index.js
@@ -0,0 +1,40 @@
+import {encriptar, desencriptar, validarTexto} from './services.js'
+
+const encriptarBtn = document.getElementById('encriptarBtn')
+const desencriptarBtn = document.getElementById('desencriptarBtn')
+const input = document.getElementById('texto-encriptar')
+const resultado = document.getElementById('texto-resultado')
+const resultadoContainer = document.getElementById('resultado-container')
+const emptyResult = document.getElementById('empty-result')
+const copiarBtn = document.getElementById('copiarBtn')
+
+
+encriptarBtn.addEventListener('click', ()=>{
+    if(!validarTexto(input.value)) {
+        alert("Texto invalido")
+        return
+    }
+
+    const textoEncriptado = encriptar(input.value)
+    resultado.innerText = textoEncriptado
+    resultadoContainer.classList.remove('hidden')
+    emptyResult.classList.add('hidden')
+})
+
+desencriptarBtn.addEventListener('click', ()=>{
+    if(!validarTexto(input.value)) {
+        alert("Texto invalido")
+        return
+    }
+    const textoDesencriptado = desencriptar(input.value)
+    resultado.innerText = textoDesencriptado
+    resultadoContainer.classList.remove('hidden')
+    emptyResult.classList.add('hidden')
+})
+
+copiarBtn.addEventListener('click',async()=>{
+    await navigator.clipboard.writeText(resultado.innerText)
+    copiarBtn.innerText = "Copiado"
+})
+
+
